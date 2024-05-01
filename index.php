b22337ba11bf1ddc8dc207ecfc03aa1fac290bce
ZenT3600 <matteo.leggio@tiscali.it>
2024-05-01 14:16:14 +0200

    Add extremely simple homepage

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <?php include 'main-imports.php';?>
+    <title>Gestionale CRUD - Home</title>
+  </head>
+  <body>
+    <h1><b>Gestionale CRUD</b></h1>
+    <h2>Homepage</h2>
+    <hr/><br>
+    <p>Tabelle:</p>
+    <ul>
+      <li><a href="read.php?tabella=songs">Songs</a></li>
+      <li><a href="read.php?tabella=artist">Artist</a></li>
+      <li><a href="read.php?tabella=albums">Albums</a></li>
+    </ul>
+  </body>
+</html>
